7003fcac33c2c5f840a0bb9b06e3424656535cb1
Roeland Jago Douma <roeland@famdouma.nl>
2017-01-04 08:59:43 +0100

    Add mail element to access list
    * Each provider just returns what they have so adding an element won't
    require changing everything
    * Added tests
    
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- tests/lib/Share20/ManagerTest.php
+++ tests/lib/Share20/ManagerTest.php
@@ -2924,7 +2924,6 @@ class ManagerTest extends \Test\TestCase {
 					'user2',
 					'user3',
 				],
-				'remote' => false,
 				'public' => true,
 			]);
 
@@ -2940,7 +2939,6 @@ class ManagerTest extends \Test\TestCase {
 					'user5',
 				],
 				'remote' => true,
-				'public' => false,
 			]);
 
 		$this->rootFolder->method('getUserFolder')
